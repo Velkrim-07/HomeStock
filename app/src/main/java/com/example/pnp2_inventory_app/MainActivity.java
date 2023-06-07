78fff81bd4d03a1ee9746f4b8f2aab4826c75c6c
Rafael Fernandes <rfernandes@eaibrasil.com>
2023-06-07 17:37:58 -0400

    DbConfig Package handles any action that will read/write into the database (C.R.U.D.).
    Features (will most likely be reworked soon)
    - added itemCreation method
    - added insertDb method
    - added initialization method
    
    The class FirebaseConfig has two private variables, one for the item in which is creating  and the other one is the db itself.
    
    Missing:
    - Case handlers for empty queries
    - Delete
    - Update
    - Read
    
    In MainActivity, it has test variables and calls which will be deleted or reworked later on.

--- app/src/main/java/com/example/pnp2_inventory_app/MainActivity.java
+++ app/src/main/java/com/example/pnp2_inventory_app/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+// map and hash for testing
+import java.util.Map;
+import java.util.HashMap;
+
+//DbConnection package!
+import DbConfig.FirebaseConfig;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseConfig dbActions = new FirebaseConfig();
+
+        // initialize stuff
+        dbActions.ConnectDatabase();
+
+        // testing create method
+        // cannot be null, make catchers
+        Map<String, Object> temp;
+        temp = dbActions.CreateItem("Fucking Milk", "Expired as fuck broski");
+
+        // testing pushToDb method
+        dbActions.InsertDb(temp);
     }
 }
