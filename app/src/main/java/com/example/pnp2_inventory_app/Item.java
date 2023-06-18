d038ef084000f74f5804ebcac0b28b98044239e5
Brandon Yates <BTYates@student.fullsail.edu>
2023-06-17 23:19:30 -0400

    Trying to get list up.

---
+++ app/src/main/java/com/example/pnp2_inventory_app/Item.java
@@ -0,0 +1,25 @@
+package com.example.pnp2_inventory_app;
+
+public class Item {
+    private String m_Name;
+    protected int m_Quantity;
+    protected String m_ExpirationDate;
+
+    public Item(String name, int quantity, String expirationDate) {
+        m_Name = name;
+        m_Quantity = quantity;
+        m_ExpirationDate = expirationDate;
+    }
+
+    public String getName() {
+        return m_Name;
+    }
+
+    public int getQuantity() {
+        return m_Quantity;
+    }
+
+    public String getExpirationDate() {
+        return m_ExpirationDate;
+    }
+}
