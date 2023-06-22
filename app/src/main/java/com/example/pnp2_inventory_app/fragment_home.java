db2763a6347d22953a91cf5c706e745569864cc5
Rafael Fernandes <rfernandes@eaibrasil.com>
2023-06-22 18:34:10 -0400

    merged some stuff from develop inside UI. DO NOT MERGE THIS INTO UI YET!

--- app/src/main/java/com/example/pnp2_inventory_app/fragment_home.java
+++ app/src/main/java/com/example/pnp2_inventory_app/fragment_home.java
@@ -13,23 +13,43 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+// DbStuff for testing
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import DbConfig.FirebaseConfig;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class fragment_home extends Fragment {
     private Button buttonEditItem;
 
+    // Rafael Testing, ignore this
+    private Button addItem;
+    private TextView items;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-// Create a sample list of items
+        // Create a sample list of items
         List<Item> itemList = new ArrayList<>();
         Item item = new Item("Corn", 5, "2023-06-30");
         itemList.add(item);
 
-// Create a custom adapter for the ListView
+        // Create a custom adapter for the ListView
         ArrayAdapter<Item> adapter = new ArrayAdapter<Item>(getContext(), R.layout.list_item_layout, itemList) {
             @NonNull
             @Override
@@ -77,5 +97,51 @@ public class fragment_home extends Fragment {
         rootView.setOnClickListener(v -> buttonEditItem.setVisibility(View.GONE));
 
         return rootView;
+    }
+
+    // Testing the usage of getAll with the new callback function
+    // will leave this here for future reference so we can take a look when we start to implement functions
+    public void testingThisShit(View view) {
+        FirebaseConfig dbActions;
+        dbActions = new FirebaseConfig();
+        dbActions.ConnectDatabase();
+
+        //scrollView testing
+        //items = view.findViewById(R.id.textView2);
+
+        addItem = view.findViewById(R.id.ButtonAddItem);
+
+        // for testing, deprecated
+        //items = view.findViewById(R.id.textView);
+
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Map<String, Object>> temp;
+
+
+                // it takes a bit of time for the Cloudstore to return the data its getting.
+                // using a callback interface (which is configured and declared inside FirebaseConfig,
+                // this will return to the function when the call returns something!
+                // currently trasnforming to json
+                // TODO: figure if we want json or just convert into item class
+                temp = dbActions.GetAll("InventoryItems", new FirebaseConfig.FirestoreCallback() {
+                    @Override
+                    public void OnCallBack(QuerySnapshot querySnapshot) {
+                        for (DocumentSnapshot document : querySnapshot.getDocuments()) {
+                            String json = document.getData().toString();
+                            List<String> test = new ArrayList<>();
+                            test.add(json);
+
+                            // testing but deprecated
+                            //items.setText(json);
+                        }
+                    }
+                });
+                //items.setText(temp.toString());
+
+
+            }
+        });
     }
 }
