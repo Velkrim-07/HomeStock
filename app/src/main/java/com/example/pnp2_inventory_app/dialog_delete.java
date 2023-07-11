34cfd5398dc3faef2a7540e16da656820cc6358b
Judah Smith-Dyer <JMSmithDyer@student.fullsail.edu>
2023-07-11 16:00:53 -0400

    Updated UI #1

--- app/src/main/java/com/example/pnp2_inventory_app/dialog_delete.java
+++ app/src/main/java/com/example/pnp2_inventory_app/dialog_delete.java
@@ -22,7 +22,6 @@ public class dialog_delete extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.dialog_delete, container, false);
     }
 
