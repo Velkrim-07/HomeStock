b849f91226d8a24f2a3e072690561c2ab856da19
Judah Smith-Dyer <JMSmithDyer@student.fullsail.edu>
2023-07-20 21:37:10 -0400

    Created the wifi adabptability now the program will save to a text file and pull from that

---
+++ app/src/main/java/com/example/pnp2_inventory_app/DateCreator.java
@@ -0,0 +1,13 @@
+package com.example.pnp2_inventory_app;
+
+public class DateCreator{
+    int m_day;
+    int m_month;
+    int m_year;
+
+    DateCreator(int day, int month, int year){
+        m_day = day;
+        m_month = month;
+        m_year = year;
+    }
+}
